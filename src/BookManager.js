0e211a056c7bad2eddcdd087b323866c70151802
Lior <spitzlior2@gmail.com>
2025-02-10 21:45:56 +0200

    Updated index.html and other necessary files

--- src/BookManager.js
+++ src/BookManager.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Button } from "./components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Input } from "./components/ui/input";
 import emailjs from "@emailjs/browser";
 
 export default function BookManager() {
